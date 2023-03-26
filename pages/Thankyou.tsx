dcef539166aeb88c39d9fec43f65f2c162f74738
dccuong <cuongksv10@mail.com>
2023-03-26 20:23:26 +0700

    vnpay return url

--- pages/Thankyou.tsx
+++ pages/Thankyou.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import crypto from "crypto";
-
+import { useDispatch, useSelector } from "react-redux";
+import { finishOrder, selectCarts, selectTotalPrice } from "../redux/cartSlice";
+import { add, addOrderDetail } from "../Api/orderApi";
+import { RootState } from "../redux/store";
+import { toast } from "react-toastify";
+import { Tuser } from "../models/user";
 type Props = {};
 
 const Thankyou = (props: Props) => {
+    const isLogged = useSelector((state: RootState) => state.auth.isLogged);
   const queryParameters = new URLSearchParams(window.location.search);
   console.log(queryParameters);
   const vnp_Amount = queryParameters.get("vnp_Amount");
@@ -19,26 +25,52 @@ const Thankyou = (props: Props) => {
   const vnp_TransactionNo = queryParameters.get("vnp_TransactionNo");
   const vnp_TransactionStatus = queryParameters.get("vnp_TransactionStatus");
 
-  const vnp_TxnRef = queryParameters.get("vnp_TxnRef");
-  console.log(vnp_BankCode,vnp_BankTranNo,vnp_CardType,vnp_OrderInfo,vnp_PayDate,vnp_ResponseCode,vnp_TmnCode,vnp_TransactionNo,vnp_TransactionStatus)
-  const signData = `${
-    `${vnp_Amount}` +
- 
-    `${vnp_BankCode}` +
-    `${vnp_BankTranNo}`+
-    `${vnp_CardType}` +
-    `${vnp_OrderInfo}` +
-    `${vnp_PayDate}`+
-    `${vnp_ResponseCode}` +
-    `${vnp_TmnCode}`+
-    `${vnp_TransactionNo}` +
-    `${vnp_TransactionStatus}`+
-    `${vnp_TxnRef}`
-  }`;
-  console.log(signData);
+  const carts = useSelector(selectCarts);
+  const totalPrice = useSelector(selectTotalPrice)   ;
+  const dispatch = useDispatch();
+  const currentUser = useSelector(
+    (state: RootState) => state.auth.currentUser
+  ) as Tuser;
+  let title=""
+  console.log(vnp_TransactionNo,vnp_TransactionStatus);
+  if(vnp_TransactionStatus=="00"){
+    title=" giao dichj thanfh coong"
+    try {
+      const data:any= localStorage.getItem("order")
+      const createOrder=async()=>{
+        const order = await add({
+            ...data,
+              totalPrice,
+              userId: isLogged ? currentUser._id : "",
+            });
+    
+            // save order detail
+            carts.forEach(async ({ productId, productPrice, quantity }) => {
+              await addOrderDetail({
+                orderId: order?._id!,
+                productId,
+                productPrice,
+                quantity,
+                pay: false,
+              });
+            });
+    
+            dispatch(finishOrder());
+            toast.success("Đặt hàng thành công");
+      }
+      createOrder()
+      } catch (error) {
+        toast.error("Có lỗi xảy ra, vui lòng thử lại");
+      }
+  }else {
+    title="giao dịch không thành công "
+  }
+  
   let hmac = crypto.createHmac("sha512", "ZPTGSZUBDGKRWWSRAEPICFEUDQSOUJMO");
-  return <div>{signData}</div>;
+  return <div>{title}</div>;
   
 };
 
 export default Thankyou;
+
+
