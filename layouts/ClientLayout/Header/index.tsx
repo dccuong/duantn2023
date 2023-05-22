e6f13c49b27d47f8ab4f99fe70aaa7fcfabb4972
Dam Nam <nam.dam@tda.company>
2023-05-22 17:28:32 +0700

    fix: fix cate #2

--- layouts/ClientLayout/Header/index.tsx
+++ layouts/ClientLayout/Header/index.tsx
@@ -268,7 +268,7 @@ const index = (props: any) => {
             </div>
 
             <ul className="bg-white hidden group-hover:block absolute top-full left-0 shadow px-2 py-1 z-[50] divide-y min-w-[150px]">
-              {cateProduct?.map((item, index) => (
+              {cateProduct?.map((item: any, index: number) => (
                 <Link href={`/prdCate/${item._id}`} key={index}>
                   <li className="text-[#282828] text-sm py-1.5 font-semibold hover:text-[#]">
                     {item.name}
@@ -309,7 +309,7 @@ const index = (props: any) => {
             </Link>
 
             <ul className="bg-white hidden group-hover:block absolute top-full left-0 shadow px-2 py-1 z-[50] divide-y min-w-[150px]">
-              {cateProduct?.map((item, index) => (
+              {cateProduct?.map((item: any, index: number) => (
                 <Link href={`/prdCate/${item._id}`} key={index}>
                   <li className="text-[#282828] text-sm py-1.5 font-semibold">
                     {item.name}
